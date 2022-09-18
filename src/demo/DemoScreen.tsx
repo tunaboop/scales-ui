61124145b4f7da88f7ad6601e76f24f506c0a8a3
Treevs <tvr.jomar@gmail.com>
2022-09-18 09:00:33 -0500

    Make initial sidebar

---
+++ src/demo/DemoScreen.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from '../logo.svg';
+import '../App.css';
+import Sidebar from "../sidebar/Sidebar";
+
+function DemoScreen() {
+  return (
+    <div className="demo-screen">
+        Demo
+        <Sidebar/>
+    </div>
+  );
+}
+
+export default DemoScreen;
---
+++ src/sidebar/Sidebar.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../App.css';
+import './Sidebar.css';
+
+function App() {
+  return (
+    <aside className="sidebar">
+      Sidebar
+    </aside>
+  );
+}
+
+export default App;
